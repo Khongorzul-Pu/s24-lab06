fd4bebc67417695473e144da632bf382ff5a7ad2
Khongorzul Pu <khongorzulk00@gmail.com>
2025-03-15 20:01:12 +0800

    Lab06

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
@@ -10,7 +10,47 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class DelegationSortedIntList {
+ public class DelegationSortedIntList implements IntegerList {
     // Write your implementation below with API documentation
+    private SortedIntList list = new SortedIntList();
+    private int totalAdded = 0;
 
+    @Override
+    public boolean add(int num) {
+        totalAdded++; // Тоог нэмэх
+        return list.add(num); // Элементийг жагсаалтад нэмэх
+    }
+
+    @Override
+    public boolean addAll(IntegerList list) {
+        boolean added = false;
+        for (int i = 0; i < list.size(); i++) {
+            added |= this.add(list.get(i)); // Элемент тус бүрийг нэмэх
+        }
+        return added;
+    }
+
+    @Override
+    public int get(int index) {
+        return list.get(index); // Жагсаалтаас элемент авах
+    }
+
+    @Override
+    public boolean remove(int num) {
+        return list.remove(num); // Элемент устгах
+    }
+
+    @Override
+    public boolean removeAll(IntegerList list) {
+        return this.list.removeAll(list); // Бүгдийг устгах
+    }
+
+    @Override
+    public int size() {
+        return list.size(); // Жагсаалтын хэмжээг буцаах
+    }
+
+    public int getTotalAdded() {
+        return totalAdded; // Нэмэгдсэн элементийн тоог буцаах
+    }
 }
